a94900a05c21fc70f45f36f3cf5bfcef5e96a7d3
Negocio <julianmartinez_93@hotmail.com>
2022-03-03 12:06:52 -0300

    implementando moda

--- Media-Mediana-Moda/calculos.js
+++ Media-Mediana-Moda/calculos.js
@@ -1,11 +1,36 @@
-let datos =[15, 35, 65, 582, 356, 96,12];
+let datos =[15, 35, 65, 356, 96,12];
+let datosOrdenados = ordenarDatos(datos)
+
+function ordenarDatos(dato){
+    var ordenados = dato.sort(function(a,b){
+        a-b
+    })
+    return ordenados
+
+
+}
 
 function media(dato){
-    let total = 0;
-    for(let i = 0; i< datos.length;i++){
+    
+    /* for(let i = 0; i< datos.length;i++){
         total = total + datos[i];   
     }
-    total = total / dato.length;
+    total = total / dato.length; */
+    let total = dato.reduce(function(valorAnterior,valorActual){
+        return valorAnterior + valorActual;
+    });
+    total /=dato.length;
     return total;
+}
+
+function moda(dato){
+
+    let mitadLista = parseInt(dato.length/2);
+    if (dato.length % 2 == 0){
+        console.log("la moda es " + (dato[mitadLista - 1] + dato[mitadLista])/2)
+    }else{
+        console.log("La moda es " + dato[mitadLista])
+    }
+
 }
 
