b84bb6bd4c327ed9f7a4d1d510d46be5f070ac73
Negocio <julianmartinez_93@hotmail.com>
2022-03-04 11:40:35 -0300

    iniciando media armonica

---
+++ Media Armonica/armonica.js
@@ -0,0 +1,23 @@
+let object = [
+    {
+        kilometros:100,
+        velocidad:100,
+    },
+    {
+        kilometros:100,
+        velocidad:20,
+    },
+    {
+        kilometros:100,
+        velocidad:100,
+    },
+    {
+        kilometros:100,
+        velocidad:130,
+    }
+]
+
+function calcularMediaArmonica(object){
+    let cantidadDatos = 4;
+    
+}
